cf98488e1009270f423852fd39efdc8294b787a1
simpsingh <155113485+simpsingh@users.noreply.github.com>
2024-04-17 05:05:32 -0400

    Create generate_text.py

---
+++ generate_text.py
@@ -0,0 +1,33 @@
+import boto3
+import json
+
+prompt_data = """
+Write SQL code to join two tables with the following DDL
+```sql
+CREATE TABLE USER
+....
+
+CREATE TABLE MORTGAGE
+....
+"""
+
+bedrock = boto3.client(service_name="bedrock-runtime")
+payload = {
+    "prompt": f"\n\nHuman:{prompt_data}\n\nAssistant:",
+    "max_tokens_to_sample": 512,
+    "temperature": 0.8,
+    "top_p": 0.8,
+}
+
+body = json.dumps(payload)
+model_id = "anthropic.claude-v2"
+response = bedrock.invoke_model(
+    body=body,
+    modelId=model_id,
+    accept="application/json",
+    contentType="application/json",
+)
+
+response_body = json.loads(response.get("body").read())
+response_text = response_body.get("completion")
+print(response_text)
