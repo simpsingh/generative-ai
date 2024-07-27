51841e8de2ce7bb8513401fc0ec345e2e6482a8b
Sim Singh <sim@simsingh.net>
2024-07-27 19:46:00 -0400

    RAG add

---
+++ src/services/rag/rag.py
@@ -0,0 +1,33 @@
+import boto3
+import json
+
+AWS_REGION_BEDROCK = "us-east-1"
+
+client = boto3.client(
+    service_name="bedrock-agent-runtime", region_name=AWS_REGION_BEDROCK
+)
+
+def handler(event, context):
+    body = json.loads(event["body"])
+    question = body.get("question")
+    if question:
+        response = client.retrieve_and_generate(
+            input={"text": question},
+            retrieveAndGenerateConfiguration={
+                "type": "KNOWLEDGE_BASE",
+                "knowledgeBaseConfiguration": {
+                    "knowledgeBaseId": "IU3P9GKGHY",
+                    "modelArn": "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-v2",
+                },
+            },
+        )
+        answer = response.get("output").get("text")
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"answer": answer}),
+        }
+    return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "question needed"}),
+        }
+
